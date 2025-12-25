60a1cb93046bacaea0a942dbf3c6748ee054db85
evan <iamevn@users.noreply.github.com>
2025-12-25 14:04:31 -0800

    compute totals as rules rather than functions

--- logic-bombs-137.rkt
+++ logic-bombs-137.rkt
@@ -5,8 +5,7 @@
 (require racklog)
 (require "possible-hits.rkt")
 
-(define TARGET-TOTAL 34)
-
+;; how many targets can each bomb of a type see?
 (define %can-see %empty-rel)
 (%assert! %can-see ()
           [('x '[#;(3 1 2) (3 2)
@@ -30,24 +29,34 @@
          (%is can-hit (possible-hits can-see))
          (%member n can-hit)]))
 
-;; how many of each bomb type are present
-(define (bomb-count bomb)
-  (second (assoc bomb '((x 2)
-                        (y 4)
-                        (z 3)
-                        (a 1)
-                        (b 1)
-                        (c 1)))))
+;; how many of a type of bomb are there?
+(define %bomb-count %empty-rel)
+(%assert! %bomb-count ()
+          [('x 2)]
+          [('y 4)]
+          [('z 3)]
+          [('a 1)]
+          [('b 1)]
+          [('c 1)])
 
-;; given numbers hit for each bomb type, return sum of targets hit
-;; by all bombs of each type
-(define (bomb-total x y z a b c)
-  (+ (* x (bomb-count 'x))
-     (* y (bomb-count 'y))
-     (* z (bomb-count 'z))
-     (* a (bomb-count 'a))
-     (* b (bomb-count 'b))
-     (* c (bomb-count 'c))))
+;; if a type of bomb hits a number of targets, how many total do all bombs of that type hit?
+(define %bomb-total
+  (%rel (bomb hit total-hit count)
+        [(bomb hit total-hit)
+         (%bomb-count bomb count)
+         (%is total-hit (* hit count))]))
+
+;; how many do all bombs hit given counts for each type?
+(define %combined-total
+  (%rel (x y z a b c total x-total y-total z-total a-total b-total c-total)
+        [(x y z a b c total)
+         (%bomb-total 'x x x-total)
+         (%bomb-total 'y y y-total)
+         (%bomb-total 'z z z-total)
+         (%bomb-total 'a a a-total)
+         (%bomb-total 'b b b-total)
+         (%bomb-total 'c c c-total)
+         (%is total (+ x-total y-total z-total a-total b-total c-total))]))
 
 ;; there's definitely a better way to write this lmao
 (define %distinct
@@ -69,11 +78,7 @@
          (%=/= a c)
          (%=/= b c)]))
 
-(define %sums-to-target
-  (%rel (x y z a b c target)
-        [(x y z a b c target)
-         (%is target (bomb-total x y z a b c))]))
-
+(define TARGET-TOTAL 34)
 ;; given hit numbers for each type of bomb and a number of targets to hit
 ;; can that many of each type of bomb be hit, do they satisify distinct rule, and do they hit the right number of targets?
 (define %hits
@@ -86,7 +91,7 @@
          (%can-hit 'b b)
          (%can-hit 'c c)
          (%distinct x y z a b c)
-         (%sums-to-target x y z a b c TARGET-TOTAL)]))
+         (%combined-total x y z a b c TARGET-TOTAL)]))
 
 #;(displayln "All possible combinations from start of puzzle")
 #;(%find-all (x y z a b c) (%hits x y z a b c))
