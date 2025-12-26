454a96bec1445b5a3d5e3d679217200de3ea3a92
evan <iamevn@users.noreply.github.com>
2025-12-25 22:27:45 -0800

    try and fail to refactor with a bomb constructor

--- logic-bombs-137.rkt
+++ logic-bombs-137.rkt
@@ -5,109 +5,76 @@
 (require racklog)
 (require "possible-hits.rkt")
 
-;; how many targets can each bomb of a type see?
-(define %can-see %empty-rel)
+(define bomb
+  ;; structure-builder for a bomb
+  (λ (name count hits can-see)
+    (list 'bomb name count hits can-see)))
+(define %bomb-name
+  (%rel (name count hits can-see)
+        [((bomb name count hits can-see) name)]))
+(define %bomb-count
+  (%rel (name count hits can-see)
+        [((bomb name count hits can-see) count)]))
+(define %bomb-hits
+  (%rel (name count hits can-see)
+        [((bomb name count hits can-see) hits)]))
+(define %bomb-can-see
+  (%rel (name count hits can-see)
+        [((bomb name count hits can-see) can-see)]))
 
-;; can a bomb 'x/'y/'z/'a/'b/'c hit given number of targets?
-(define %can-hit
-  (%rel (bomb n can-see can-hit)
-        [(bomb n)
-         (%can-see bomb can-see)
-         (%is can-hit (possible-hits can-see))
-         (%member n can-hit)]))
+(define %bomb-can-hit
+  (%rel (bomb can-see can-hit)
+        [(bomb can-hit)
+         (%bomb-can-see bomb can-see)
+         (%is can-hit (possible-hits can-see))]))
 
-;; how many of a type of bomb are there?
-(define %bomb-count %empty-rel)
-
-;; if a type of bomb hits a number of targets, how many total do all bombs of that type hit?
 (define %bomb-total
-  (%rel (bomb hit total-hit count)
-        [(bomb hit total-hit)
+  (%rel (bomb count hits total)
+        [(bomb total)
          (%bomb-count bomb count)
-         (%is total-hit (* hit count))]))
-
-;; how many do all bombs hit given counts for each type?
+         (%bomb-hits bomb hits)
+         (%is total (* count hits))]))
 (define %combined-total
-  (%rel (x y z a b c total x-total y-total z-total a-total b-total c-total)
-        [(x y z a b c total)
-         (%bomb-total 'x x x-total)
-         (%bomb-total 'y y y-total)
-         (%bomb-total 'z z z-total)
-         (%bomb-total 'a a a-total)
-         (%bomb-total 'b b b-total)
-         (%bomb-total 'c c c-total)
-         (%is total (+ x-total y-total z-total a-total b-total c-total))]))
+  (%rel (bombs total totals)
+        [(bombs total)
+         (%let (b n)
+               (%bag-of n (%bomb-total b n) totals))
+         (%is total (apply + totals))]))
 
-;; %true if list has no repeated elements
-(define %distinct/list
+(define %distinct
+  ;; %true for lists containing no duplicate elements
   (%rel (l h t)
         [('()) %true]
         [((cons h t))
          (%not (%member h t))
-         (%distinct/list t)]))
+         (%distinct t)]))
+(define %hits-distinct
+  (%rel (bombs hits)
+        [(bombs)
+         (%let (b n)
+               (%bag-of n (%bomb-hits b n) hits))
+         (%distinct hits)]))
 
-;; %true if x y z a b c are distinct
-(define %distinct
-  (%rel (x y z a b c)
-        [(x y z a b c)
-         (%distinct/list (list x y z a b c))]))
-
-;; given hit numbers for each type of bomb and a number of targets to hit
-;; can that many of each type of bomb be hit, do they satisify distinct rule, and do they hit the right number of targets?
-(define %hits
-  (%rel (x y z a b c total)
-        [(x y z a b c total)
-         (%can-hit 'x x)
-         (%can-hit 'y y)
-         (%can-hit 'z z)
-         (%can-hit 'a a)
-         (%can-hit 'b b)
-         (%can-hit 'c c)
-         (%distinct x y z a b c)
-         (%combined-total x y z a b c total)]))
-
-(define (mk-bomb name count can-see)
-  (%assert! %bomb-count () [(name count)])
-  (%assert! %can-see () [(name can-see)]))
-
-(define (naiive-bombs)
-  (mk-bomb 'x 2 '[(3 1 2)
-                  (3 1 2)])
-  (mk-bomb 'y 4 '[(3 3 3)
-                  (3 1 {1 2})
-                  (3 3 3)
-                  (1 2 2)])
-  (mk-bomb 'z 3 '[({1 2} 1 3)
-                  (1 3 3)
-                  ({1 2} 1 2)])
-  (mk-bomb 'a 1 '[(3 1 {1 2})])
-  (mk-bomb 'b 1 '[(3 3 1)])
-  (mk-bomb 'c 1 '[(2 1 3)]))
-
-(define (refined-bombs)
-  (mk-bomb 'x 2 '[(3 2)
-                  (3 1 2)])
-  (mk-bomb 'y 4 '[(3 3 3)
-                  (3 1 {1 2})
-                  (3 3 3)
-                  (1 2 2)])
-  (mk-bomb 'z 3 '[({1 2} 1 3)
-                  (1 3)
-                  ({1 2} 2)])
-  (mk-bomb 'a 1 '[(3 1 {1 2})])
-  (mk-bomb 'b 1 '[(3 3 1)])
-  (mk-bomb 'c 1 '[(2 1 3)]))
-
-(refined-bombs)
-(define TARGETS 34)
-
-#;(displayln "All possible combinations from start of puzzle")
-#;(%find-all (x y z a b c) (%hits x y z a b c TARGETS))
-#;(displayln "")
 (%find-all (x y z a b c)
-           (%and (%hits x y z a b c TARGETS)
-                 (%>= x 2)
-                 (%>= y 3)
-                 (%>= z 1)
-                 (%>= a 2)
-                 (%>= c 2)))
+           (%and
+            (%>= x 2)
+            (%>= y 3)
+            (%>= z 1)
+            (%>= a 2)
+            (%>= c 2)
+            (%let (bx by bz ba bb bc)
+                  (%is bx (bomb 'x 2 x '[(3 2)
+                                         (3 1 2)]))
+                  (%is by (bomb 'y 4 y '[(3 3 3)
+                                         (3 1 {1 2})
+                                         (3 3 3)
+                                         (1 2 2)]))
+                  (%is bz (bomb 'z 3 z '[({1 2} 1 3)
+                                         (1 3)
+                                         ({1 2} 2)]))
+                  (%is ba (bomb 'a 1 a '[(3 1 {1 2})]))
+                  (%is bb (bomb 'b 1 b '[(3 3 1)]))
+                  (%is bc (bomb 'c 1 c '[(2 1 3)]))
+                  (let ([bombs (list bx by bz ba bb bc)])
+                    (%hits-distinct bombs)
+                    (%combined-total bombs 34)))))
